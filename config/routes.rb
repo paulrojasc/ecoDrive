448e77c559e7dddb2ac32927976519340a515373
Paul Rojas <paulrojasc@gmail.com>
2023-10-27 04:17:40 -0300

    Proyecto inicial con plantilla

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "pages#home"
+
+  # Vistas estaticas
+  get '/home',    to: 'pages#home'
+  get '/about', to: 'pages#about'
+  get '/terms',   to: 'pages#terms'
+end
